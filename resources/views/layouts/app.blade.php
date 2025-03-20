d0a6c50981ff981a48552faa0f0c7dc47da2e385
Lahiru Jayawardhane <93923625+lahirujay99@users.noreply.github.com>
2025-03-20 16:07:29 +0530

    change navbar appearence

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased ">
-    <div class="min-h-screen bg-gray-100 flex flex-col">
+    <div class="min-h-screen flex flex-col">
           <x-navigation/>
 
             <!-- Page Heading -->
