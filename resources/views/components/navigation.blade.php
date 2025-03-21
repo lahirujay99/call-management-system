92b63a254c7869438b225c9b68be2742a51195c3
Lahiru Jayawardhane <93923625+lahirujay99@users.noreply.github.com>
2025-03-21 15:31:53 +0530

    fix navbar issue

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -2,16 +2,18 @@
     <div class="container mx-auto px-4 flex justify-between items-center">
 
         <div class="flex items-center justify-between w-full md:w-auto">
-            <div class="bg-gray-900 text-white rounded-full px-6 py-3 font-semibold hover:bg-gray-700 cursor-pointer md:hidden">
+            <div
+                class="bg-gray-900 text-white rounded-full px-6 py-3 font-semibold hover:bg-gray-700 cursor-pointer md:hidden">
                 Logo
             </div>
             <div class="hidden md:block md:bg-gray-900 md:rounded-full md:hover:bg-gray-700 md:cursor-pointer">
-                <img src="{{ asset('images/icon.jpeg') }}" alt="Website Logo" class="h-12 w-auto rounded-full" />
+                <img src="{{ asset('images/icon.jpeg') }}" alt="Website Logo" class="h-12 w-auto rounded-full"/>
             </div>
 
 
             <button id="mobile-menu-button" class="md:hidden focus:outline-none" aria-label="Toggle mobile menu">
-                <svg class="w-6 h-6 text-gray-700 hover:text-gray-900" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                <svg class="w-6 h-6 text-gray-700 hover:text-gray-900" fill="none" stroke="currentColor"
+                     viewBox="0 0 24 24"
                      xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
                     </path>
@@ -35,15 +37,25 @@
                         <p class="cursor-pointer text-white hover:opacity-[0.9]">Advanced-Search</p>
                     </div>
                 @endif
-
             </nav>
         </div>
 
-        <div class="hidden md:block bg-gray-900 text-white rounded-full px-6 py-3 font-semibold hover:bg-gray-700 cursor-pointer">
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
+        <div class="flex flex-row gap-5 bg-black px-6 py-2 font-semibold rounded-full">
+            <div class="text-white px-2 py-1">
+                @if(auth()->check() && auth()->user()->isAdmin())
+                    Admin
+                @elseif(auth()->check() && auth()->user()->isOperator())
+                    User
+                @else
+                    User
+                @endif
+            </div>
+            <div>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="bg-white rounded-full text-black px-2 py-1">Logout</button>
+                </form>
+            </div>
         </div>
 
 
@@ -61,9 +73,19 @@
                     <p class="block py-2 cursor-pointer text-gray-700 hover:bg-gray-100">Admin</p>
                 @endif
                 <div class="py-2">
-                    <div class="bg-gray-900 text-white rounded-full px-6 py-3 font-semibold hover:bg-gray-700 cursor-pointer text-center">
+                    <div
+                        class="bg-gray-900 text-white rounded-full px-6 py-3 font-semibold hover:bg-gray-700 cursor-pointer text-center">
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
+                            <span>
+                                @if(auth()->check() && auth()->user()->isAdmin())
+                                    Admin
+                                @elseif(auth()->check() && auth()->user()->isOperator())
+                                    User
+                                @else
+                                    User
+                                @endif
+                            </span>
                             <button type="submit">Logout</button>
                         </form>
                     </div>
